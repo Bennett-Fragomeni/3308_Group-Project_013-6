8bd279f7929ad048e06a4165aabad27de3a1828b
Nicholas Hellmers <nihe1661@colorado.edu>
2022-11-03 00:18:33 -0600

    made initial setup for db

---
+++ src/init_data/create.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password CHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS recipes CASCADE;
+CREATE TABLE recipes(
+    recipe_id SERIAL PRIMARY KEY,
+    recipe_name VARCHAR(50) NOT NULL,
+    recipe_desc VARCHAR(500) NOT NULL,
+    password CHAR(60) NOT NULL,
+    user_id SERIAL NOT NULL
+);
+
+DROP TABLE IF EXISTS ingredients CASCADE;
+CREATE TABLE ingredients(
+    ingredient_id SERIAL PRIMARY KEY,
+    ingredient_name VARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS recipe_to_ingredients CASCADE;
+CREATE TABLE recipe_to_ingredients(
+    recipe_id SERIAL NOT NULL,
+    ingredient_id SERIAL NOT NULL
+);
