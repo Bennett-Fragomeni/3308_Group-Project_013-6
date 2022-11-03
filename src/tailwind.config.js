9eb050a185025b859854577848f939307637c31f
Nathan Keyt <nake8894@colorado.edu>
2022-11-02 20:24:48 -0600

    edited dist, package.json, package-lock.json, .env

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/views/pages/register.html", "./src/views/partials/header.ejs", "./src/views/partials/footer.ejs"],
   theme: {
